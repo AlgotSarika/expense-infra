befec9659468d4a5b9e4c830b15822af2e1107da
Algot Sarika <sarika@joindevops.com>
2025-06-14 00:19:00 +0530

    expense-infra

--- 00-vpc/output.tf
+++ 00-vpc/output.tf
@@ -2,9 +2,9 @@
     value = module.vpc.azs_info
 } */
 
-# output "subnets_info" {
-#     value = module.vpc.subnets_info
-# }
+output "subnets_info" {
+    value = module.vpc.subnets_info
+}
 output "public_subnet_ids" {
     value = module.vpc.public_subnet_ids
 }
